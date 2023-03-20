eb013516caac4b66bdb68efe21aa7acc307a583a
LucyMkr <lucymkrtchyan52@gmail.com>
2023-03-21 00:48:10 +0400

    inheritance 2.0

--- Cards/src/Main.java
+++ Cards/src/Main.java
@@ -5,13 +5,17 @@ public class Main {
         System.out.println(card1.getSum());
         card1.getMoney(15000);
         System.out.println(card1.getSum());
-        Business card = new Business("1547 3584 5415 1546", "1547", "541", 150000);
-        card.duty();
-        System.out.println(card.getSum1());
         Gold card2 = new Gold("1547 3584 5415 1546", "1547", "541", 1000000);
         System.out.println(card2.getLunchSum());
         card2.lunchMoney(15000);
         System.out.println(card2.getLunchSum());
         Gold card4 = new Gold("1547 3584 5415 1546", "5752", "154", 100);
+        System.out.println("problem of one million starts");
+        Business card = new Business("1547 3584 5415 1546", "1547", "541", 150000);
+        card.duty();
+        System.out.println(card.getSum1());
+        card.duty();
+        System.out.println(card.getSum1());
+
     }
 }
